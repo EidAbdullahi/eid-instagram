571b8936971c3db2cd92315d85b95b4d99d0f1fb
EidAbdullahi <eidabdullahi10@gmail.com>
2021-01-18 10:38:58 +0300

    commit changes

--- instagram/migrations/0001_initial.py
+++ instagram/migrations/0001_initial.py
@@ -80,4 +80,4 @@ class Migration(migrations.Migration):
             name='user',
             field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
         ),
-    ].
+    ]
